850fc979cbbac15ba6720b35ac0087ed439e2052
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-12-08 10:10:39 -0300

    commit bia

--- vagas/vagas.js
+++ vagas/vagas.js
@@ -1,143 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('lerMaisBtn');
-    var requisitos = document.getElementById('requisitosTexto');
+    var lerMaisBtn = document.querySelector('.lerMaisBtn');
+    var lerMenosBtn = document.querySelector('.lerMenosBtn');
+    var requisitosTexto = document.getElementById('requisitosTexto');
     var oferecemos = document.getElementById('oferecemos');
-    var horarios = document.getElementById('horarios');
+    var horarios = document.getElementById('horarios'); 
     var isExpanded = false;
 
-    button.addEventListener('click', function () {
+    lerMaisBtn.addEventListener('click', function () {
         isExpanded = !isExpanded;
 
         if (isExpanded) {
-            button.textContent = 'Ler Menos';
-            requisitos.classList.remove('oculto');
+            requisitosTexto.classList.remove('oculto');
             oferecemos.classList.remove('oculto');
             horarios.classList.remove('oculto');
+            lerMaisBtn.classList.add('oculto');
+            lerMenosBtn.classList.remove('oculto');
         } else {
-            button.textContent = 'Ler Mais';
-            requisitos.classList.add('oculto');
+            requisitosTexto.classList.remove('oculto');
             oferecemos.classList.add('oculto');
             horarios.classList.add('oculto');
+            lerMaisBtn.classList.remove('oculto');
+            lerMenosBtn.classList.add('oculto');
         }
     });
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('lerMaisBtn1');
-    var requisitos = document.getElementById('requisitosTexto1');
-    var oferecemos = document.getElementById('oferecemos1');
-    var horarios = document.getElementById('horarios1');
-    var isExpanded = false;
-
-    button.addEventListener('click', function () {
-        isExpanded = !isExpanded;
-
-        if (isExpanded) {
-            button.textContent = 'Ler Menos';
-            requisitos.classList.remove('oculto1');
-            oferecemos.classList.remove('oculto1');
-            horarios.classList.remove('oculto1');
-        } else {
-            button.textContent = 'Ler Mais';
-            requisitos.classList.add('oculto1');
-            oferecemos.classList.add('oculto1');
-            horarios.classList.add('oculto1');
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('lerMaisBtn2');
-    var requisitos = document.getElementById('requisitosTexto2');
-    var oferecemos = document.getElementById('oferecemos2');
-    var horarios = document.getElementById('horarios2');
-    var isExpanded = false;
-
-    button.addEventListener('click', function () {
-        isExpanded = !isExpanded;
-
-        if (isExpanded) {
-            button.textContent = 'Ler Menos';
-            requisitos.classList.remove('oculto2');
-            oferecemos.classList.remove('oculto2');
-            horarios.classList.remove('oculto2');
-        } else {
-            button.textContent = 'Ler Mais';
-            requisitos.classList.add('oculto2');
-            oferecemos.classList.add('oculto2');
-            horarios.classList.add('oculto2');
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('lerMaisBtn3');
-    var requisitos = document.getElementById('requisitosTexto3');
-    var oferecemos = document.getElementById('oferecemos3');
-    var horarios = document.getElementById('horarios3');
-    var isExpanded = false;
-
-    button.addEventListener('click', function () {
-        isExpanded = !isExpanded;
-
-        if (isExpanded) {
-            button.textContent = 'Ler Menos';
-            requisitos.classList.remove('oculto3');
-            oferecemos.classList.remove('oculto3');
-            horarios.classList.remove('oculto3');
-        } else {
-            button.textContent = 'Ler Mais';
-            requisitos.classList.add('oculto3');
-            oferecemos.classList.add('oculto3');
-            horarios.classList.add('oculto3');
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('lerMaisBtn4');
-    var requisitos = document.getElementById('requisitosTexto4');
-    var oferecemos = document.getElementById('oferecemos4');
-    var horarios = document.getElementById('horarios4');
-    var isExpanded = false;
-
-    button.addEventListener('click', function () {
-        isExpanded = !isExpanded;
-
-        if (isExpanded) {
-            button.textContent = 'Ler Menos';
-            requisitos.classList.remove('oculto4');
-            oferecemos.classList.remove('oculto4');
-            horarios.classList.remove('oculto4');
-        } else {
-            button.textContent = 'Ler Mais';
-            requisitos.classList.add('oculto4');
-            oferecemos.classList.add('oculto4');
-            horarios.classList.add('oculto4');
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('lerMaisBtn5');
-    var requisitos = document.getElementById('requisitosTexto5');
-    var oferecemos = document.getElementById('oferecemos5');
-    var horarios = document.getElementById('horarios5');
-    var isExpanded = false;
-
-    button.addEventListener('click', function () {
-        isExpanded = !isExpanded;
-
-        if (isExpanded) {
-            button.textContent = 'Ler Menos';
-            requisitos.classList.remove('oculto5');
-            oferecemos.classList.remove('oculto5');
-            horarios.classList.remove('oculto5');
-        } else {
-            button.textContent = 'Ler Mais';
-            requisitos.classList.add('oculto5');
-            oferecemos.classList.add('oculto5');
-            horarios.classList.add('oculto5');
-        }
+    lerMenosBtn.addEventListener('click', function () {
+        requisitosTexto.classList.add('oculto');
+        oferecemos.classList.add('oculto');
+        horarios.classList.add('oculto');
+        lerMaisBtn.classList.remove('oculto');
+        lerMenosBtn.classList.add('oculto');
+        isExpanded = false;
     });
 });
