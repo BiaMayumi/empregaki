accdedf784d727f083733cbafe9ea0e6e0a1a516
joohzinato <joao.sobral@sptech.school>
2023-12-13 22:57:06 -0300

    commit zinatto

--- artigo/index2.php
+++ artigo/index2.php
@@ -23,9 +23,9 @@
             <img src="img/tendencias-do-mercado-de-trabalho.png" alt="" class="img6">
 
             <h3 class="texto1">
-  Descubra algumas tendências mais quentes do mercadodetrabalho e aprenda a se preparar para elas. Neste artigo falo sobre as habilidades e conhecimentos necessários para se destacar em um ambiente profissional em constante evolução. Saiba como adaptar-se às novas demandas e posicione-se para o sucesso! <br><br>
+  Descubra algumas tendências mais quentes do mercadodetrabalho e aprenda a se preparar para <br>elas. Neste artigo falo sobre as habilidades e conhecimentos necessários para se destacar em um <br>ambiente profissional em constante evolução. Saiba como adaptar-se às novas demandas e <br>posicione-se para o sucesso! <br><br>
 
-  Você já parou para pensar como o mercado de trabalho está em constante transformação? À medida que a tecnologia avança e as necessidades das empresas mudam, é fundamental ficar atualizado sobre as tendências emergentes e se preparar para elas. Hoje vamos explorar as tendências mais quentes do mercadodetrabalho e fornecer insights sobre como você pode se destacar e se adaptar a essas mudanças.
+  Você já parou para pensar como o mercado de trabalho está em constante transformação? À <br>medida que a tecnologia avança e as necessidades das empresas mudam, é fundamental ficar <br>atualizado sobre as tendências emergentes e se preparar para elas. Hoje vamos explorar as <br>tendências mais quentes do mercadodetrabalho e fornecer insights sobre como você pode se<br> destacar e se adaptar a essas mudanças.
 
 </h3>
 
@@ -55,21 +55,21 @@
 
 
     <h3 class="texto2">Automação e #InteligênciaArtificial (IA)<br><br>
-A #automação e a inteligência artificial estão moldando o futuro do trabalho. Descubra como essas tecnologias estão revolucionando diversos setores e aprenda as habilidades que serão valorizadas nesse contexto.
+A #automação e a inteligência artificial estão moldando o futuro do trabalho. Descubra como essas <br>tecnologias estão revolucionando diversos setores e aprenda as habilidades que serão valorizadas<br> nesse contexto.
 
-A automação e a inteligência artificial estão cada vez mais presentes em diversos setores da economia. Tarefas repetitivas e previsíveis estão sendo automatizadas, o que pode levar à redução de alguns empregos. No entanto, a automação também cria novas oportunidades de trabalho, como o desenvolvimento e a manutenção de sistemas de IA, a adaptação de processos às novas tecnologias e a criação de soluções inovadoras.<br><br>
+A automação e a inteligência artificial estão cada vez mais presentes em diversos <br>setores da economia. Tarefas repetitivas e previsíveis estão sendo automatizadas, o que pode levar<br> à redução de alguns empregos. No entanto, a automação também cria novas oportunidades de<br> trabalho, como o desenvolvimento e a manutenção de sistemas de IA, a adaptação de processos <br>às novas tecnologias e a criação de soluções inovadoras.<br><br>
 
-Para se preparar para essa tendência, é essencial desenvolver habilidades que complementem a automação, como pensamento crítico, criatividade, habilidades de resolução de problemas e capacidade de trabalhar em equipe. Além disso, buscar conhecimento em áreas relacionadas à inteligência artificial e automação, como ciência de dados e programação, pode abrir portas para novas oportunidades.
+Para se preparar para essa tendência, é essencial desenvolver habilidades que complementem a<br> automação, como pensamento crítico, criatividade, habilidades de resolução de problemas e<br> capacidade de trabalhar em equipe. Além disso, buscar conhecimento em áreas relacionadas à <br>inteligência artificial e automação, como ciência de dados e programação, pode abrir portas para <br>novas oportunidades.
   
 Trabalhoremoto e Freelancer<br><br>
-O trabalho remoto e a economia freelancer estão ganhando força em todo o mundo. A pandemia de COVID-19 acelerou a adoção do trabalho remoto e a tendência de contratação de profissionais freelancers. As empresas perceberam que muitas atividades podem ser realizadas fora do escritório tradicional, o que oferece mais flexibilidade tanto para empregadores quanto para funcionários. Além disso, o acesso a talentos globais por meio de plataformas de freelancers ampliou o alcance das empresas.
+O trabalho remoto e a economia freelancer estão ganhando força em todo o mundo. A pandemia de<br> COVID-19 acelerou a adoção do trabalho remoto e a tendência de contratação de profissionais <br>freelancers. As empresas perceberam que muitas atividades podem ser realizadas fora do <br>escritório tradicional, o que oferece mais flexibilidade tanto para empregadores quanto para funcionários. Além disso, o acesso a talentos globais por meio de plataformas de freelancers <br>ampliou o alcance das empresas.
 
-Para se adaptar a esse cenário, é importante desenvolver habilidades de comunicação virtual, gerenciamento de tempo e autodisciplina. Além disso, a construção de uma boa rede de contatos profissionais, tanto presencialmente quanto online, é fundamental para encontrar oportunidades de trabalho remoto ou projetos freelance.<br><br><br>
+Para se adaptar a esse cenário, é importante desenvolver <br>habilidades de comunicação virtual, gerenciamento de tempo e autodisciplina. Além disso, a<br> construção de uma boa rede de contatos profissionais, tanto presencialmente quanto online, é<br> fundamental para encontrar oportunidades de trabalho remoto ou projetos freelance.<br><br><br>
 
 Habilidades Digitais e #TransformaçãoDigital<br><br>
-A transformação digital está impactando todos os setores da #economia. A #economiadigital e o #comércioeletrônico estão em constante expansão. Cada vez mais pessoas estão realizando compras online e empresas de todos os setores estão se adaptando a essa demanda. Além disso, novas profissões surgiram nesse contexto, como marketing digital, desenvolvimento de aplicativos e gestão de redes sociais.
+A transformação digital está impactando todos os setores da #economia. A #economiadigital e o <br>#comércioeletrônico estão em constante expansão. Cada vez mais pessoas estão realizando<br> compras online e empresas de todos os setores estão se adaptando a essa demanda. Além disso, <br>novas profissões surgiram nesse contexto, como marketing digital, desenvolvimento de aplicativos <br>e gestão de redes sociais.
 
-Para se preparar para essa tendência, é importante adquirir habilidades relacionadas à tecnologia, como marketing digital, análise de dados e desenvolvimento web. O conhecimento das tendências do comércio eletrônico e a capacidade de se adaptar rapidamente às novas plataformas e ferramentas também são essenciais.
+Para se preparar para essa tendência, é importante adquirir habilidades <br>relacionadas à tecnologia, como marketing digital, análise de dados e desenvolvimento web. O <br>conhecimento das tendências do comércio eletrônico e a capacidade de se adaptar rapidamente às <br>novas plataformas e ferramentas também são essenciais.
 </h3>
 
 
--- artigo/index3.php
+++ artigo/index3.php
@@ -23,60 +23,60 @@
             <img src="img/pexels-cottonbro-5989933.webp" alt="" class="img6">
 
             <h3 class="texto1">
-            Preparar-se adequadamente para uma entrevista de emprego é essencial para impressionar os entrevistadores e aumentar suas chances de sucesso. Aqui estão algumas estratégias para ajudar você a se sair bem em entrevistas:<br><br>
+            Preparar-se adequadamente para uma entrevista de emprego é essencial para impressionar os <br>entrevistadores e aumentar suas chances de sucesso. Aqui estão algumas estratégias para ajudar <br>você a se sair bem em entrevistas:<br><br>
 
 Antes da Entrevista:<br><br>
 Pesquise a Empresa:<br>
 
 Conheça a empresa, sua cultura, valores e produtos/serviços oferecidos.
-Entenda o mercado em que a empresa atua e suas principais concorrências.<br><br>
->Estude a Descrição da Vaga:<br>
+Entenda o mercado em <br>que a empresa atua e suas principais concorrências.<br><br>
+Estude a Descrição da Vaga:<br>
 
 Analise a descrição da vaga para entender as habilidades e qualidades desejadas.
-Prepare exemplos específicos de como suas habilidades se alinham com os requisitos da vaga.<br><br>
+Prepare<br> exemplos específicos de como suas habilidades se alinham com os requisitos da vaga.<br><br>
 Pratique Respostas para Perguntas Comuns:<br>
 
 Antecipe perguntas comuns de entrevistas e pratique suas respostas.
-Destaque suas realizações passadas e como podem ser aplicadas à nova posição.<br><br>
+Destaque suas realizações <br>passadas e como podem ser aplicadas à nova posição.<br><br>
 Prepare Perguntas para os Entrevistadores:<br>
 
-Prepare algumas perguntas sobre a empresa, a equipe e as expectativas para demonstrar seu interesse.
+Prepare algumas perguntas sobre a empresa, a equipe e as expectativas para demonstrar seu <br>interesse.
 Evite perguntas que podem ser facilmente respondidas com uma pesquisa básica.<br><br>
 Escolha uma Roupa Adequada:<br>
 
 Vista-se de forma profissional e adequada à cultura da empresa.
-Certifique-se de que suas roupas estejam limpas e em bom estado.<br><br>
+Certifique-se de que suas roupas<br> estejam limpas e em bom estado.<br><br>
 Durante a Entrevista:<br><br>
 Demonstre Confiança e Profissionalismo:<br>
 
-Mantenha uma postura ereta, faça contato visual e cumprimente os entrevistadores com firmeza.
+Mantenha uma postura ereta, faça contato visual e cumprimente os entrevistadores com firmeza.<br>
 Demonstre confiança ao falar sobre suas habilidades e experiências.<br><br>
 Comunique-se de Forma Clara e Concisa:<br>
 
 Evite respostas muito longas ou vagas. Seja claro e direto.
-Use exemplos concretos para ilustrar suas realizações e habilidades.<br><br>
+Use exemplos concretos para ilustrar<br> suas realizações e habilidades.<br><br>
 Mostre Entusiasmo e Interesse:<br>
 
 Demonstre seu entusiasmo pela oportunidade e pela empresa.
-Fale sobre como suas habilidades e objetivos se alinham aos da empresa.<br><br>
+Fale sobre como suas habilidades e <br>objetivos se alinham aos da empresa.<br><br>
 Lide com Perguntas Comportamentais:<br>
 
-Responda a perguntas comportamentais usando a estrutura STAR (Situação, Tarefa, Ação, Resultado).
+Responda a perguntas comportamentais usando a estrutura STAR (Situação, Tarefa, Ação,<br> Resultado).
 Destaque situações desafiadoras que você superou no passado.<br><br>
 Seja Transparente sobre suas Experiências:<br>
 
 Seja honesto sobre suas experiências e habilidades.
-Caso não saiba a resposta para uma pergunta, admita e destaque seu desejo de aprender.<br><br>
+Caso não saiba a resposta para uma<br> pergunta, admita e destaque seu desejo de aprender.<br><br>
 Após a Entrevista:<br><br>
 Envie um E-mail de Agradecimento:<br>
 
-Envie um e-mail de agradecimento após a entrevista, expressando seu apreço pela oportunidade.
+Envie um e-mail de agradecimento após a entrevista, expressando seu apreço pela oportunidade.<br>
 Reforce seu interesse na posição e na empresa.<br><br>
 Avalie seu Desempenho:<br>
 
 Faça uma reflexão sobre como foi a entrevista e o que poderia ser melhorado.
-Use essa avaliação para se preparar melhor para futuras entrevistas.<br><br>
-Lembre-se de que cada entrevista é uma oportunidade de aprendizado, mesmo que não resulte em uma oferta de emprego. A prática constante e a adaptação às diferentes empresas e entrevistadores o ajudarão a se destacar nas entrevistas futuras.
+Use essa avaliação <br>para se preparar melhor para futuras entrevistas.<br><br>
+Lembre-se de que cada entrevista é uma oportunidade de aprendizado, mesmo que não resulte em <br>uma oferta de emprego. A prática constante e a adaptação às diferentes empresas e <br>entrevistadores o ajudarão a se destacar nas entrevistas futuras.
 </h3>
 
 
